f83fe1bb0f39fbbbe99e6c5db667b9d493b738fa
Edward <jaysongraphics@gmail.com>
2021-06-30 08:43:38 -0400

    commit tree fc92d952da30c7ef9138894e063115827e6ee12b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
-  end
+      def sentence?
+        self.end_with?('.')
+      end
 
-  def question?
+      def question?
+        self.end_with?('?')
+      end
 
-  end
+      def exclamation?
+        self.end_with?('!')
+      end
 
-  def exclamation?
+      def count_sentences
+        self.split(/\.|\?|\!/).size
+        # binding.pry
+      end
 
-  end
-
-  def count_sentences
-
-  end
 end
